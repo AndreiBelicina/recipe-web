bff92a11b187cc6caa9d2d900a19984ff1ec7222
Belicina, Andrei Lester <andreilester.belicina15@gmail.com>
2025-01-24 12:52:12 +0800

    auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const {userLogin, userSignUp, getUser} = require("../controllers/user")
-const verifyToken = require("./middleware/verifyToken");
-
+const verifyToken = require("../middleware/auth");
 
 router.post("/signUp", userSignUp)
-router.post("/login", userLogin)
+router.post("/login", userLogin, verifyToken)
 router.get("/user/:id", getUser)
 
 module.exports = router
